244f4156ed3f526c20281a65dfef28e5bfd9a74b
Valerii Maslenykov <code@getjoystick.com>
2023-05-04 20:10:35 +0200

    Use sha256 library to support browser environment

--- src/internals/hash/sha256-to-hex.ts
+++ src/internals/hash/sha256-to-hex.ts
@@ -1,11 +1,4 @@
-import { Crypto } from "@peculiar/webcrypto";
-
-const crypto = new Crypto();
-
-const toHex = (hashBuffer: ArrayBuffer): string =>
-  Array.from(new Uint8Array(hashBuffer))
-    .map((b) => b.toString(16).padStart(2, "0"))
-    .join("");
+import sha256 from "crypto-js/sha256";
 
 /**
  * The sha256ToHex function takes a string or array of unknowns and returns the SHA-256 hash of that data as a hexadecimal string.
@@ -19,12 +12,7 @@ const toHex = (hashBuffer: ArrayBuffer): string =>
 export async function sha256ToHex<TData extends string | unknown[]>(
   data: TData
 ): Promise<string> {
-  const massagedData = typeof data === "string" ? data : JSON.stringify(data);
+  const dataToBeHashed = typeof data === "string" ? data : JSON.stringify(data);
 
-  const hashBuffer = await crypto.subtle.digest(
-    "SHA-256",
-    new TextEncoder().encode(massagedData)
-  );
-
-  return toHex(hashBuffer);
+  return sha256(dataToBeHashed).toString();
 }
